689fc6966d887fdef028fa6528434f0dec9d2670
alexandrapetrea1 <alexandra.petrea1@stud.ubbcluj.ro>
2024-10-13 19:36:54 +0300

    1

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int[] grades = {29, 37, 38, 41, 84, 67};
+
         Aufgabe1 aufgabe1 = new Aufgabe1();
+
         System.out.println("Failing grades: " + aufgabe1.getFailingGrades(grades));
         System.out.println("Average grade: " + aufgabe1.calculateAverage(grades));
         System.out.println("Rounded grades: ");
@@ -11,5 +13,14 @@ public class Main {
         System.out.println();
         System.out.println("Maximum rounded grade: " + aufgabe1.getMax(grades));
 
+
+        int[] numbers = {4, 8, 3, 10, 17};
+
+        Aufgabe2 aufgabe2 = new Aufgabe2();
+
+        System.out.println("Maximum number: " + aufgabe2.findMax(numbers));
+        System.out.println("Minimum number: " + aufgabe2.findMin(numbers));
+        System.out.println("Maximum sum of n-1 numbers: " + aufgabe2.findMaxSum(numbers));
+        System.out.println("Minimum sum of n-1 numbers: " + aufgabe2.findMinSum(numbers));
     }
 }
